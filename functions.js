3e8bbe2e701e931087bde297af18c65595276490
Slimymaks <maksnaboka1@gmail.com>
2025-02-20 19:58:16 -0500

    github button added

---
+++ functions.js
@@ -0,0 +1,11 @@
+function sendUserTo(link){
+    window.location.href = link;
+}
+
+// function showText(id, show){
+    // if(show){
+    //     document.getElementById(id).hidden = false;
+    // }else{
+    //     document.getElementById(id).hidden = true;
+    // }
+// }
